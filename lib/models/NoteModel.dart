378ffc763c1f60e1d35328b3c2baac80127b1833
shrouk <sheroukhasssan@gmail.com>
2023-09-17 22:56:51 +0300

    Generate Type adapter

--- lib/models/NoteModel.dart
+++ lib/models/NoteModel.dart
@@ -1,8 +1,17 @@
+import 'package:hive/hive.dart';
+part 'NoteModel.g.dart';
+
+@HiveType(typeId: 0)
 class NoteModel {
+  @HiveField(0)
   String? title;
+  @HiveField(1)
   String? subTitle;
+  @HiveField(2)
   String? Date;
+  @HiveField(3)
   int? color;
+
   NoteModel({
     this.title,
     this.subTitle,
